b77f5dbc2dec034cd3e8f3d20e39c64bcf5ba059
BuildTools <unconfigured@null.spigotmc.org>
2017-07-16 09:07:22 +0200

    fix

--- src/com/mrsweeter/dreamAPI/Configuration/PluginConfiguration.java
+++ src/com/mrsweeter/dreamAPI/Configuration/PluginConfiguration.java
@@ -2,10 +2,7 @@ package com.mrsweeter.dreamAPI.Configuration;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
