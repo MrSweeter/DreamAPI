95c2f3ec6f5413d548c56296f5a824f7e8ed84dc
BuildTools <unconfigured@null.spigotmc.org>
2017-07-29 15:20:11 +0200

    Add event for personnal test

--- src/com/mrsweeter/dreamAPI/DreamAPI.java
+++ src/com/mrsweeter/dreamAPI/DreamAPI.java
@@ -1,5 +1,9 @@
 package com.mrsweeter.dreamAPI;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -7,9 +11,20 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author MrSweeter
  *
  */
-public class DreamAPI extends JavaPlugin	{
+public class DreamAPI extends JavaPlugin implements Listener	{
 	
-	public void onEnable()	{}
+	public void onEnable()	{
+		
+		Bukkit.getPluginManager().registerEvents(this, this);
+		
+	}
 	
 	public void onDisable()	{}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event)	{
+		
+		
+		
+	}
 }
